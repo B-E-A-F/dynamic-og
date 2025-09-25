44e0f35230bb2b95f64ade5a934d6c922a3eb0e7
bea <bea@a4r.dev>
2025-09-25 11:14:55 -0700

    ✨ Add bitekarma logo to the main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="flex flex-col items-center justify-center">
+	<img src="Logo-light-lg.png" alt="Logo" class="h-auto" />
+</div>
